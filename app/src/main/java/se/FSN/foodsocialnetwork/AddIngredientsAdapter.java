7a51ee628e060239bb3bc2c53488d07a0f64c226
JulioLopez <juliojlgon@gmail.com>
2014-12-15 16:38:18 +0100

    Almost Working Post recipe. Working in add Ingredients

--- app/src/main/java/se/FSN/foodsocialnetwork/AddIngredientsAdapter.java
+++ app/src/main/java/se/FSN/foodsocialnetwork/AddIngredientsAdapter.java
@@ -1,31 +1,73 @@
 package se.FSN.foodsocialnetwork;
 
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by JulioLopez on 15/12/14.
  */
 public class AddIngredientsAdapter extends BaseAdapter {
 
+    private Activity activity;
+    private LayoutInflater inflater;
+    private List<Ingredient> ingredientsItems;
+
+
+    public AddIngredientsAdapter(Activity activity, List<Ingredient> ingredientsItems) {
+        this.activity = activity;
+        this.ingredientsItems = ingredientsItems;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return ingredientsItems.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return ingredientsItems.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        if (inflater == null)
+            inflater = (LayoutInflater) activity
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null)
+            convertView = inflater.inflate(R.layout.ingredientslist_pattern, null);
+
+
+        TextView title = (TextView) convertView.findViewById(R.id.ingredientTitle);
+        TextView quantity = (TextView) convertView.findViewById(R.id.quantity);
+        TextView type = (TextView) convertView.findViewById(R.id.unitType);
+
+        // getting movie data for the row
+        Ingredient i = ingredientsItems.get(position);
+
+        // title
+        title.setText(i.getTitle());
+
+        // quantity
+        quantity.setText("Quantity: " + i.getQuantity());
+
+        // Input Type
+        type.setText(i.getInputType());
+
+        return convertView;
     }
+
+    ;
 }
+
